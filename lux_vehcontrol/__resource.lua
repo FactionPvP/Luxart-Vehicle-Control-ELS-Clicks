4712bec7666e506c3ba67bec67c7b1d8925b7da0
Faction <thefactionpvpmc@gmail.com>
2021-08-20 16:21:42 -0500

    Fixed __resource.lua

--- lux_vehcontrol/__resource.lua
+++ lux_vehcontrol/__resource.lua
@@ -1,13 +1,13 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
-ui_page('html/index.html')
+ui_page('index.html')
 
 files({
     'index.html',
-	'html/sounds/On.ogg',
-	'html/sounds/Upgrade.ogg',
-	'html/sounds/Off.ogg',
-	'html/sounds/Beep.ogg',
-	'html/sounds/Downgrade.ogg'
+	'sounds/On.ogg',
+	'sounds/Upgrade.ogg',
+	'sounds/Off.ogg',
+	'sounds/Beep.ogg',
+	'sounds/Downgrade.ogg'
 })
 
 client_script 'client.lua'
